2f78886f5a697bbb312aef04f18b5250f4efc1e1
Shruti-12-S <shruti21.shinde@gmail.com>
2026-03-07 10:18:11 +0530

    stripe added

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -2,7 +2,8 @@ import express from "express"
 import { checkAvailabilityAPI, 
     createBooking, 
     getHotelBookings, 
-    getUserBookings} from "../controllers/bookingController.js";
+    getUserBookings,
+    stripePayment} from "../controllers/bookingController.js";
 import {protect} from "../middleware/authMiddleware.js"
 
 const bookingRouter = express.Router();
@@ -11,5 +12,7 @@ bookingRouter.post('/checkAvailability',checkAvailabilityAPI);
 bookingRouter.post('/book', protect, createBooking);
 bookingRouter.get('/user', protect, getUserBookings);
 bookingRouter.get('/hotel', protect, getHotelBookings);
+
+bookingRouter.post('/stripe-payment', protect, stripePayment);
 
 export default bookingRouter;
